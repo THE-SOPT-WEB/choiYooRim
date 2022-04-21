176dcdacfd99edfb0614dfe42e3a8a6c0ecde050
yoorimChoi <yulim3839@naver.com>
2022-04-21 23:38:38 +0900

    🍔 [refactor] : 불필요한 코드 정리

--- week2/stronghamsik/main.js
+++ week2/stronghamsik/main.js
@@ -5,7 +5,6 @@ let accumulatedAmount = 0;
 
 function clickBurgerCard(){
   const cardList = $('.mac__burger');
-  console.log(cardList);
   const shoppingList = $('.shopping__list');
   // const cart = $('shopping__list')
   cardList.addEventListener('click',(e)=>{  
@@ -17,13 +16,11 @@ function clickBurgerCard(){
     }
     
     for(let burger of burgerList){
-      if(burger.name === burgerInfo.name.innerText){ //이미 안에 버거가 있는 종류라면 
-        console.log(burger);
+      if(burger.name === burgerInfo.name.innerText){ 
         burger.amount++;
         const burgerName = "." + burgerInfo.name.innerText;
         const oldBurger = document.querySelector(burgerName);
         oldBurger.querySelector('input').value = burger.amount;
-        console.log(burgerList);
         calcPrice();
         return;
       }
@@ -34,9 +31,9 @@ function clickBurgerCard(){
       price: burgerInfo.price.innerText,
       amount : 1,
     }
+
     burgerList.push(newBurger);
     calcPrice();
-    console.log(burgerList);
     //장바구니에 직접 추가를 해주는 부분 -> 뷰를 그리기 위함
     shoppingList.innerHTML += `
       <div class="added__burger">
@@ -64,7 +61,6 @@ function showModal(modalContent){
 function clickNoButton(){
   const noButton = $('.no');
   noButton.addEventListener('click',(e)=>{
-    console.log('no버튼 선택됨');
     hideModal();
   })
 }
@@ -82,14 +78,12 @@ function clickOrderButton(){
   const orderButton = $('.order__button');
   
   orderButton.addEventListener('click',(e)=>{
-    console.log("버튼 클릭됨");
     showModal('정말 주문하시겠어요?');
   })
 }
 
 const parsePriceToNumber = (price) => {
 	const removedComma = price.replace(/\D/g, "");
-  console.log(removedComma);
   return +removedComma;
 };
 
@@ -105,10 +99,8 @@ function calcPrice(){
 function clearCart(){
   const cancelButton = $('.cancel__button');
   cancelButton.addEventListener('click',(e)=>{
-    console.log('취소하기 버튼 클릭됨');
     const shoppingList = $('.shopping__list');
     burgerList = [];
-    console.log(burgerList);
     shoppingList.innerHTML = `
     `;
     accumulatedAmount = 0;
@@ -121,9 +113,7 @@ function deleteBurger(){
   for(let i = 0; i<xButton.length; i++){
     xButton[i].addEventListener('click',(e)=>{
       const xBurger = e.target.closest('.added__burger');
-      console.log(xBurger.innerHTML);
       burgerList = burgerList.filter((element) => element.name !== xBurger.querySelector('.burger__name').innerText);
-      console.log(burgerList);
       calcPrice();
       xBurger.innerHTML = ``;
     })
@@ -137,24 +127,14 @@ function controlAmount(){
       const countBurger = e.target.closest('.added__burger');
       const count = e.target.closest('.burger__amount');
       const countBurgerName = countBurger.querySelector('.burger__name').innerText;
-      console.log(countBurger.innerHTML);
-      console.log(count.value);
-      console.log(countBurgerName);
       for(let burger of burgerList){
         if(burger.name === countBurgerName){ //이미 안에 버거가 있는 종류라면 
-          console.log(burger);
           burger.amount = Number(count.value);
-          console.log(burger);
         }
       }
       calcPrice();
-      // burgerList = burgerList.filter((element) => element.name !== xBurger.querySelector('.burger__name').innerText);
-      // console.log(burgerList);
-      // calcPrice();
-      // xBurger.innerHTML = ``;
     })
   }
-
 }
 
 window.onload = () =>{
@@ -162,8 +142,5 @@ window.onload = () =>{
   clickOrderButton();
   clickNoButton();
   clearCart();
-  if(burgerList.length !== 0){
-    controlAmount();
-  }
 }
 
